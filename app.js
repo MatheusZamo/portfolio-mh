c32f8d7c02e50688f9ad3e7829d0d842862f68be
Matheus Henrique <19matheushenrique03@gmail.com>
2024-08-06 19:53:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // classe que adiciona uma posição fixa no topo, para a nav, 'fixed-top'
 
+const typing = document.querySelector('[data-js="typing"]')
+
+const messages = ['Front-end','Freelancer','Web']
+
+let messageIndex = 0
+let characterIndex = 0
+let currentMessage = ''
+let currentCharacters = ''
+
+const type = () => {
+  const shouldTypeFirstMessage = messageIndex === messages.length
+
+  if(shouldTypeFirstMessage){
+    messageIndex = 0
+  }
+
+  currentMessage = messages[messageIndex]
+  currentCharacters = currentMessage.slice(0, characterIndex++)
+  typing.textContent = currentCharacters
+
+  const shouldChangeMessageToBeTyped = 
+    currentCharacters.length === currentMessage.length
+
+  if(shouldChangeMessageToBeTyped){
+    messageIndex++
+    characterIndex = 0
+  }
+  console.log(currentCharacters)
+}
+
+//setInterval(type, 300)
